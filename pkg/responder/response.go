5ea35550fa595e25723084a55c7fb3db31f43d89
Aris Ripandi <riipandi@gmail.com>
2026-09-12 09:40:36 +0700

    refactor: modernize Go 1.27

--- pkg/responder/response.go
+++ pkg/responder/response.go
@@ -71,9 +71,7 @@ type RateLimit struct {
 type Links map[string]*string
 
 // Link returns a pointer for a Links value; a nil entry renders as null.
-//
-//go:fix inline
-func Link(url string) *string { return new(url) }
+func Link(url string) *string { return &url }
 
 // Option customizes the envelope built by Success and Fail.
 type Option func(*Envelope)
@@ -115,7 +113,7 @@ func WithLink(rel, url string) Option {
 		if e.Links == nil {
 			e.Links = Links{}
 		}
-		e.Links[rel] = new(url)
+		e.Links[rel] = Link(url)
 	}
 }
 
